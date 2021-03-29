208e1e65094b9676833628eb35403bebdca46d48
Faustino Zanetto <faustizanetto@gmail.com>
2021-03-28 23:27:43 -0300

    fix

--- src/auth/sendRefreshToken.ts
+++ src/auth/sendRefreshToken.ts
@@ -1,9 +1,10 @@
 import { Response } from 'express';
-import { __prod__ } from '../utils/constants';
+import { __origin__, __prod__ } from '../utils/constants';
 
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('jid', token, {
     httpOnly: true,
     path: '/refresh_token',
+    signed: false,
   });
 };
